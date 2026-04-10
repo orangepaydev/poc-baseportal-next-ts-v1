e7ef46b54f2914f1acebe115a7c074d4400762bf
orangepaydev <orangepaydev@gmail.com>
2026-04-10 16:26:36 +0800

    add country code

--- docker-init/mariadb/init/003-portaldb-records.sql
+++ docker-init/mariadb/init/003-portaldb-records.sql
@@ -195,6 +195,283 @@ FROM (
 INNER JOIN system_codes system_code
   ON system_code.system_code = 'CurrencyCode';
 
+INSERT IGNORE INTO system_codes (
+  system_code,
+  description,
+  status
+) VALUES (
+  'CountryCode',
+  'List of countries as per ISO 3166-1 alpha-2 two-letter codes',
+  'ACTIVE'
+);
+
+INSERT IGNORE INTO system_code_values (
+  system_code_id,
+  system_code_value,
+  description,
+  status,
+  sort_order
+)
+SELECT
+  system_code.id,
+  seed.system_code_value,
+  seed.description,
+  'ACTIVE',
+  seed.sort_order
+FROM (
+  SELECT 'AF' AS system_code_value, 'Afghanistan' AS description, 1 AS sort_order
+  UNION ALL SELECT 'AX', 'Åland Islands', 2
+  UNION ALL SELECT 'AL', 'Albania', 3
+  UNION ALL SELECT 'DZ', 'Algeria', 4
+  UNION ALL SELECT 'AS', 'American Samoa', 5
+  UNION ALL SELECT 'AD', 'Andorra', 6
+  UNION ALL SELECT 'AO', 'Angola', 7
+  UNION ALL SELECT 'AI', 'Anguilla', 8
+  UNION ALL SELECT 'AQ', 'Antarctica', 9
+  UNION ALL SELECT 'AG', 'Antigua and Barbuda', 10
+  UNION ALL SELECT 'AR', 'Argentina', 11
+  UNION ALL SELECT 'AM', 'Armenia', 12
+  UNION ALL SELECT 'AW', 'Aruba', 13
+  UNION ALL SELECT 'AU', 'Australia', 14
+  UNION ALL SELECT 'AT', 'Austria', 15
+  UNION ALL SELECT 'AZ', 'Azerbaijan', 16
+  UNION ALL SELECT 'BS', 'Bahamas', 17
+  UNION ALL SELECT 'BH', 'Bahrain', 18
+  UNION ALL SELECT 'BD', 'Bangladesh', 19
+  UNION ALL SELECT 'BB', 'Barbados', 20
+  UNION ALL SELECT 'BY', 'Belarus', 21
+  UNION ALL SELECT 'BE', 'Belgium', 22
+  UNION ALL SELECT 'BZ', 'Belize', 23
+  UNION ALL SELECT 'BJ', 'Benin', 24
+  UNION ALL SELECT 'BM', 'Bermuda', 25
+  UNION ALL SELECT 'BT', 'Bhutan', 26
+  UNION ALL SELECT 'BO', 'Bolivia, Plurinational State of', 27
+  UNION ALL SELECT 'BQ', 'Bonaire, Sint Eustatius and Saba', 28
+  UNION ALL SELECT 'BA', 'Bosnia and Herzegovina', 29
+  UNION ALL SELECT 'BW', 'Botswana', 30
+  UNION ALL SELECT 'BV', 'Bouvet Island', 31
+  UNION ALL SELECT 'BR', 'Brazil', 32
+  UNION ALL SELECT 'IO', 'British Indian Ocean Territory', 33
+  UNION ALL SELECT 'BN', 'Brunei Darussalam', 34
+  UNION ALL SELECT 'BG', 'Bulgaria', 35
+  UNION ALL SELECT 'BF', 'Burkina Faso', 36
+  UNION ALL SELECT 'BI', 'Burundi', 37
+  UNION ALL SELECT 'KH', 'Cambodia', 38
+  UNION ALL SELECT 'CM', 'Cameroon', 39
+  UNION ALL SELECT 'CA', 'Canada', 40
+  UNION ALL SELECT 'CV', 'Cape Verde', 41
+  UNION ALL SELECT 'KY', 'Cayman Islands', 42
+  UNION ALL SELECT 'CF', 'Central African Republic', 43
+  UNION ALL SELECT 'TD', 'Chad', 44
+  UNION ALL SELECT 'CL', 'Chile', 45
+  UNION ALL SELECT 'CN', 'China', 46
+  UNION ALL SELECT 'CX', 'Christmas Island', 47
+  UNION ALL SELECT 'CC', 'Cocos (Keeling) Islands', 48
+  UNION ALL SELECT 'CO', 'Colombia', 49
+  UNION ALL SELECT 'KM', 'Comoros', 50
+  UNION ALL SELECT 'CG', 'Congo', 51
+  UNION ALL SELECT 'CD', 'Congo, the Democratic Republic of the', 52
+  UNION ALL SELECT 'CK', 'Cook Islands', 53
+  UNION ALL SELECT 'CR', 'Costa Rica', 54
+  UNION ALL SELECT 'CI', 'Côte d''Ivoire', 55
+  UNION ALL SELECT 'HR', 'Croatia', 56
+  UNION ALL SELECT 'CU', 'Cuba', 57
+  UNION ALL SELECT 'CW', 'Curaçao', 58
+  UNION ALL SELECT 'CY', 'Cyprus', 59
+  UNION ALL SELECT 'CZ', 'Czech Republic', 60
+  UNION ALL SELECT 'DK', 'Denmark', 61
+  UNION ALL SELECT 'DJ', 'Djibouti', 62
+  UNION ALL SELECT 'DM', 'Dominica', 63
+  UNION ALL SELECT 'DO', 'Dominican Republic', 64
+  UNION ALL SELECT 'EC', 'Ecuador', 65
+  UNION ALL SELECT 'EG', 'Egypt', 66
+  UNION ALL SELECT 'SV', 'El Salvador', 67
+  UNION ALL SELECT 'GQ', 'Equatorial Guinea', 68
+  UNION ALL SELECT 'ER', 'Eritrea', 69
+  UNION ALL SELECT 'EE', 'Estonia', 70
+  UNION ALL SELECT 'ET', 'Ethiopia', 71
+  UNION ALL SELECT 'FK', 'Falkland Islands (Malvinas)', 72
+  UNION ALL SELECT 'FO', 'Faroe Islands', 73
+  UNION ALL SELECT 'FJ', 'Fiji', 74
+  UNION ALL SELECT 'FI', 'Finland', 75
+  UNION ALL SELECT 'FR', 'France', 76
+  UNION ALL SELECT 'GF', 'French Guiana', 77
+  UNION ALL SELECT 'PF', 'French Polynesia', 78
+  UNION ALL SELECT 'TF', 'French Southern Territories', 79
+  UNION ALL SELECT 'GA', 'Gabon', 80
+  UNION ALL SELECT 'GM', 'Gambia', 81
+  UNION ALL SELECT 'GE', 'Georgia', 82
+  UNION ALL SELECT 'DE', 'Germany', 83
+  UNION ALL SELECT 'GH', 'Ghana', 84
+  UNION ALL SELECT 'GI', 'Gibraltar', 85
+  UNION ALL SELECT 'GR', 'Greece', 86
+  UNION ALL SELECT 'GL', 'Greenland', 87
+  UNION ALL SELECT 'GD', 'Grenada', 88
+  UNION ALL SELECT 'GP', 'Guadeloupe', 89
+  UNION ALL SELECT 'GU', 'Guam', 90
+  UNION ALL SELECT 'GT', 'Guatemala', 91
+  UNION ALL SELECT 'GG', 'Guernsey', 92
+  UNION ALL SELECT 'GN', 'Guinea', 93
+  UNION ALL SELECT 'GW', 'Guinea-Bissau', 94
+  UNION ALL SELECT 'GY', 'Guyana', 95
+  UNION ALL SELECT 'HT', 'Haiti', 96
+  UNION ALL SELECT 'HM', 'Heard Island and McDonald Islands', 97
+  UNION ALL SELECT 'VA', 'Holy See (Vatican City State)', 98
+  UNION ALL SELECT 'HN', 'Honduras', 99
+  UNION ALL SELECT 'HK', 'Hong Kong', 100
+  UNION ALL SELECT 'HU', 'Hungary', 101
+  UNION ALL SELECT 'IS', 'Iceland', 102
+  UNION ALL SELECT 'IN', 'India', 103
+  UNION ALL SELECT 'ID', 'Indonesia', 104
+  UNION ALL SELECT 'IR', 'Iran, Islamic Republic of', 105
+  UNION ALL SELECT 'IQ', 'Iraq', 106
+  UNION ALL SELECT 'IE', 'Ireland', 107
+  UNION ALL SELECT 'IM', 'Isle of Man', 108
+  UNION ALL SELECT 'IL', 'Israel', 109
+  UNION ALL SELECT 'IT', 'Italy', 110
+  UNION ALL SELECT 'JM', 'Jamaica', 111
+  UNION ALL SELECT 'JP', 'Japan', 112
+  UNION ALL SELECT 'JE', 'Jersey', 113
+  UNION ALL SELECT 'JO', 'Jordan', 114
+  UNION ALL SELECT 'KZ', 'Kazakhstan', 115
+  UNION ALL SELECT 'KE', 'Kenya', 116
+  UNION ALL SELECT 'KI', 'Kiribati', 117
+  UNION ALL SELECT 'KP', 'Korea, Democratic People''s Republic of', 118
+  UNION ALL SELECT 'KR', 'Korea, Republic of', 119
+  UNION ALL SELECT 'KW', 'Kuwait', 120
+  UNION ALL SELECT 'KG', 'Kyrgyzstan', 121
+  UNION ALL SELECT 'LA', 'Lao People''s Democratic Republic', 122
+  UNION ALL SELECT 'LV', 'Latvia', 123
+  UNION ALL SELECT 'LB', 'Lebanon', 124
+  UNION ALL SELECT 'LS', 'Lesotho', 125
+  UNION ALL SELECT 'LR', 'Liberia', 126
+  UNION ALL SELECT 'LY', 'Libya', 127
+  UNION ALL SELECT 'LI', 'Liechtenstein', 128
+  UNION ALL SELECT 'LT', 'Lithuania', 129
+  UNION ALL SELECT 'LU', 'Luxembourg', 130
+  UNION ALL SELECT 'MO', 'Macao', 131
+  UNION ALL SELECT 'MK', 'Macedonia, the Former Yugoslav Republic of', 132
+  UNION ALL SELECT 'MG', 'Madagascar', 133
+  UNION ALL SELECT 'MW', 'Malawi', 134
+  UNION ALL SELECT 'MY', 'Malaysia', 135
+  UNION ALL SELECT 'MV', 'Maldives', 136
+  UNION ALL SELECT 'ML', 'Mali', 137
+  UNION ALL SELECT 'MT', 'Malta', 138
+  UNION ALL SELECT 'MH', 'Marshall Islands', 139
+  UNION ALL SELECT 'MQ', 'Martinique', 140
+  UNION ALL SELECT 'MR', 'Mauritania', 141
+  UNION ALL SELECT 'MU', 'Mauritius', 142
+  UNION ALL SELECT 'YT', 'Mayotte', 143
+  UNION ALL SELECT 'MX', 'Mexico', 144
+  UNION ALL SELECT 'FM', 'Micronesia, Federated States of', 145
+  UNION ALL SELECT 'MD', 'Moldova, Republic of', 146
+  UNION ALL SELECT 'MC', 'Monaco', 147
+  UNION ALL SELECT 'MN', 'Mongolia', 148
+  UNION ALL SELECT 'ME', 'Montenegro', 149
+  UNION ALL SELECT 'MS', 'Montserrat', 150
+  UNION ALL SELECT 'MA', 'Morocco', 151
+  UNION ALL SELECT 'MZ', 'Mozambique', 152
+  UNION ALL SELECT 'MM', 'Myanmar', 153
+  UNION ALL SELECT 'NA', 'Namibia', 154
+  UNION ALL SELECT 'NR', 'Nauru', 155
+  UNION ALL SELECT 'NP', 'Nepal', 156
+  UNION ALL SELECT 'NL', 'Netherlands', 157
+  UNION ALL SELECT 'NC', 'New Caledonia', 158
+  UNION ALL SELECT 'NZ', 'New Zealand', 159
+  UNION ALL SELECT 'NI', 'Nicaragua', 160
+  UNION ALL SELECT 'NE', 'Niger', 161
+  UNION ALL SELECT 'NG', 'Nigeria', 162
+  UNION ALL SELECT 'NU', 'Niue', 163
+  UNION ALL SELECT 'NF', 'Norfolk Island', 164
+  UNION ALL SELECT 'MP', 'Northern Mariana Islands', 165
+  UNION ALL SELECT 'NO', 'Norway', 166
+  UNION ALL SELECT 'OM', 'Oman', 167
+  UNION ALL SELECT 'PK', 'Pakistan', 168
+  UNION ALL SELECT 'PW', 'Palau', 169
+  UNION ALL SELECT 'PS', 'Palestine, State of', 170
+  UNION ALL SELECT 'PA', 'Panama', 171
+  UNION ALL SELECT 'PG', 'Papua New Guinea', 172
+  UNION ALL SELECT 'PY', 'Paraguay', 173
+  UNION ALL SELECT 'PE', 'Peru', 174
+  UNION ALL SELECT 'PH', 'Philippines', 175
+  UNION ALL SELECT 'PN', 'Pitcairn', 176
+  UNION ALL SELECT 'PL', 'Poland', 177
+  UNION ALL SELECT 'PT', 'Portugal', 178
+  UNION ALL SELECT 'PR', 'Puerto Rico', 179
+  UNION ALL SELECT 'QA', 'Qatar', 180
+  UNION ALL SELECT 'RE', 'Réunion', 181
+  UNION ALL SELECT 'RO', 'Romania', 182
+  UNION ALL SELECT 'RU', 'Russian Federation', 183
+  UNION ALL SELECT 'RW', 'Rwanda', 184
+  UNION ALL SELECT 'BL', 'Saint Barthélemy', 185
+  UNION ALL SELECT 'SH', 'Saint Helena, Ascension and Tristan da Cunha', 186
+  UNION ALL SELECT 'KN', 'Saint Kitts and Nevis', 187
+  UNION ALL SELECT 'LC', 'Saint Lucia', 188
+  UNION ALL SELECT 'MF', 'Saint Martin (French part)', 189
+  UNION ALL SELECT 'PM', 'Saint Pierre and Miquelon', 190
+  UNION ALL SELECT 'VC', 'Saint Vincent and the Grenadines', 191
+  UNION ALL SELECT 'WS', 'Samoa', 192
+  UNION ALL SELECT 'SM', 'San Marino', 193
+  UNION ALL SELECT 'ST', 'Sao Tome and Principe', 194
+  UNION ALL SELECT 'SA', 'Saudi Arabia', 195
+  UNION ALL SELECT 'SN', 'Senegal', 196
+  UNION ALL SELECT 'RS', 'Serbia', 197
+  UNION ALL SELECT 'SC', 'Seychelles', 198
+  UNION ALL SELECT 'SL', 'Sierra Leone', 199
+  UNION ALL SELECT 'SG', 'Singapore', 200
+  UNION ALL SELECT 'SX', 'Sint Maarten (Dutch part)', 201
+  UNION ALL SELECT 'SK', 'Slovakia', 202
+  UNION ALL SELECT 'SI', 'Slovenia', 203
+  UNION ALL SELECT 'SB', 'Solomon Islands', 204
+  UNION ALL SELECT 'SO', 'Somalia', 205
+  UNION ALL SELECT 'ZA', 'South Africa', 206
+  UNION ALL SELECT 'GS', 'South Georgia and the South Sandwich Islands', 207
+  UNION ALL SELECT 'SS', 'South Sudan', 208
+  UNION ALL SELECT 'ES', 'Spain', 209
+  UNION ALL SELECT 'LK', 'Sri Lanka', 210
+  UNION ALL SELECT 'SD', 'Sudan', 211
+  UNION ALL SELECT 'SR', 'Suriname', 212
+  UNION ALL SELECT 'SJ', 'Svalbard and Jan Mayen', 213
+  UNION ALL SELECT 'SZ', 'Eswatini', 214
+  UNION ALL SELECT 'SE', 'Sweden', 215
+  UNION ALL SELECT 'CH', 'Switzerland', 216
+  UNION ALL SELECT 'SY', 'Syrian Arab Republic', 217
+  UNION ALL SELECT 'TW', 'Taiwan, Province of China', 218
+  UNION ALL SELECT 'TJ', 'Tajikistan', 219
+  UNION ALL SELECT 'TZ', 'Tanzania, United Republic of', 220
+  UNION ALL SELECT 'TH', 'Thailand', 221
+  UNION ALL SELECT 'TL', 'Timor-Leste', 222
+  UNION ALL SELECT 'TG', 'Togo', 223
+  UNION ALL SELECT 'TK', 'Tokelau', 224
+  UNION ALL SELECT 'TO', 'Tonga', 225
+  UNION ALL SELECT 'TT', 'Trinidad and Tobago', 226
+  UNION ALL SELECT 'TN', 'Tunisia', 227
+  UNION ALL SELECT 'TR', 'Turkey', 228
+  UNION ALL SELECT 'TM', 'Turkmenistan', 229
+  UNION ALL SELECT 'TC', 'Turks and Caicos Islands', 230
+  UNION ALL SELECT 'TV', 'Tuvalu', 231
+  UNION ALL SELECT 'UG', 'Uganda', 232
+  UNION ALL SELECT 'UA', 'Ukraine', 233
+  UNION ALL SELECT 'AE', 'United Arab Emirates', 234
+  UNION ALL SELECT 'GB', 'United Kingdom', 235
+  UNION ALL SELECT 'US', 'United States', 236
+  UNION ALL SELECT 'UM', 'United States Minor Outlying Islands', 237
+  UNION ALL SELECT 'UY', 'Uruguay', 238
+  UNION ALL SELECT 'UZ', 'Uzbekistan', 239
+  UNION ALL SELECT 'VU', 'Vanuatu', 240
+  UNION ALL SELECT 'VE', 'Venezuela, Bolivarian Republic of', 241
+  UNION ALL SELECT 'VN', 'Viet Nam', 242
+  UNION ALL SELECT 'VG', 'Virgin Islands, British', 243
+  UNION ALL SELECT 'VI', 'Virgin Islands, U.S.', 244
+  UNION ALL SELECT 'WF', 'Wallis and Futuna', 245
+  UNION ALL SELECT 'EH', 'Western Sahara', 246
+  UNION ALL SELECT 'YE', 'Yemen', 247
+  UNION ALL SELECT 'ZM', 'Zambia', 248
+  UNION ALL SELECT 'ZW', 'Zimbabwe', 249
+) AS seed
+INNER JOIN system_codes system_code
+  ON system_code.system_code = 'CountryCode';
+
 INSERT IGNORE INTO users (
   organization_id,
   username,
